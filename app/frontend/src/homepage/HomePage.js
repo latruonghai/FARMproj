aa1c1394c5c7dd6c53802576ae3d438a37a5085d
latruonghai@gmail.com <latruonghai@gmail.com>
2021-08-14 18:40:21 +0700

    Update NavBar and handleClick to login buton

--- app/frontend/src/homepage/HomePage.js
+++ app/frontend/src/homepage/HomePage.js
@@ -1,11 +1,14 @@
 import Login from './Login';
+import React from 'react';
+import ToolBox from './ToolBox';
+import "./css/HomePage.css";
 
 export default function HomePage(){
 
-
-    return(
+    return (
         <div>
+            <ToolBox/>
             <Login/>
         </div>
-    )
+    );
 }
